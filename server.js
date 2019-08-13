de04c9def67ab6a0cc7ee6a3edd996bda408e2e2
Marvin <44047272+marvinxu99@users.noreply.github.com>
2019-08-12 20:07:00 -0700

    adding PORT

--- server.js
+++ server.js
@@ -38,5 +38,5 @@ app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
 
 //app.listen(3001, ()=>{
 app.listen(process.env.PORT || 3001, ()=>{
-  console.log(`app is running on port {process.env.PORT}`);
+  console.log(`app is running on port ${process.env.PORT}`);
 });
